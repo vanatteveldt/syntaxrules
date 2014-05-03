0306a2925d2b42776bd7bb9d4a5187295a5f9b41
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-05-03 21:14:55 +0200

    Keep sparqlrunner resident

--- sparqlrunner/SparqlRunner.java
+++ sparqlrunner/SparqlRunner.java
@@ -13,15 +13,39 @@ import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.update.*;
 import javax.json.*;
 
-
 public class SparqlRunner {
-    public static void main(String[] args) throws Exception{
-	JsonReader reader = Json.createReader(new InputStreamReader(System.in));
-	JsonObject json = (JsonObject) reader.read();
-	System.err.println("SparqlRunner: Reading model from STDIN");
-	// Read data
-	Model m = ModelFactory.createDefaultModel() ;
-        m.read(new StringReader(json.getString("data")), null, "N3") ;
+    
+    private BufferedReader input;
+    private Model model;
+    private boolean done = false;
+    
+    public SparqlRunner() {
+	input = new BufferedReader(new InputStreamReader(System.in));
+	model =  ModelFactory.createDefaultModel();
+    }
+	
+
+    String readJson() throws Exception {
+	StringBuilder buffer = new StringBuilder();
+	while (true) {
+	    String line = input.readLine();
+	    if (line == null) done = true;
+	    if (line == null || line.equals(""))
+		return buffer.toString();
+	    buffer.append(line);
+	}
+    }
+
+    void handle(String jsonstr) throws Exception  {
+	long t_start = System.currentTimeMillis();
+
+	JsonObject json = (JsonObject) Json.createReader(new StringReader(jsonstr)).read();
+	long t_decoded = System.currentTimeMillis();
+
+	model.removeAll();
+	long t_cleared = System.currentTimeMillis();
+        model.read(new StringReader(json.getString("data")), null, "N3") ;
+	long t_loaded = System.currentTimeMillis();
 
 	// Perform updates
 	UpdateRequest request = UpdateFactory.create() ;
@@ -36,12 +60,41 @@ public class SparqlRunner {
 	    }
 	    
 	}
-	UpdateAction.execute(request, m) ;
+	long t_requested = System.currentTimeMillis();
+	UpdateAction.execute(request, model) ;
+	long t_executed = System.currentTimeMillis();
 	
 	// Output results
 	System.err.println("SparqlRunner: Writing model to STDOUT");
-	m.write(System.out, "NT");
+	model.write(System.out, "NT");
+
+	long t_done = System.currentTimeMillis();
+
+	System.err.println("Decoding: "+(t_decoded - t_start));
+	System.err.println("Cleared model: "+(t_cleared - t_decoded));
+	System.err.println("Loaded model: "+(t_loaded - t_cleared));
+	System.err.println("Creating request: "+(t_requested - t_loaded));
+	System.err.println("Executing request: "+(t_executed - t_requested));
+	System.err.println("Writing output: "+(t_done - t_executed));
+       
 	System.err.println("SparqlRunner: DONE");
-	
+	System.err.println("SparqlRunner: READY");
+	System.out.println();
+    }
+
+    void go() throws Exception {
+	System.err.println("SparqlRunner: READY");
+	System.out.println();
+	while (!done) {
+	    String json = readJson();
+	    if (json != null && json.length() > 0)
+		handle(json);
+	}
+	System.err.println("SparqlRunner: EOF -> DONE!");
+
+    }
+
+    public static void main(String[] args) throws Exception {
+	new SparqlRunner().go();
     }
 }
--- syntaxrules/sparqlrunner.py
+++ syntaxrules/sparqlrunner.py
@@ -2,40 +2,105 @@
 Run a number of sparql update statements on a data source
 Uses the simple java script SparqlRunner.java to address JENA ARQ
 """
-
+import logging
 import os, os.path
 import json
 import subprocess
 from rdflib import Graph
+import threading
+import functools
+
+def read_lines(source, target):
+    """Read lines from file source and appends them to list target, until EOF or empty line"""
+    while True:
+        line = source.readline()
+        if not (line and line.strip()):
+            break
+        target.append(line)
+
+class SparqlRunner(object):
+
+    
+    def __init__(self):
+        self.start_sparqlrunner()
+
+    def start_sparqlrunner(self):
+        jena = os.path.abspath(os.environ['JENA_HOME'])
+        import syntaxrules
+        runner = os.path.abspath(os.path.join(os.path.dirname(syntaxrules.__file__), "../sparqlrunner"))
+        classpath = "{jena}/lib/*:{runner}:{runner}/javax.json-1.0.4.jar".format(**locals())
+        cmd = "java -cp '{classpath}' SparqlRunner".format(**locals())
+        self.p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.communicate(input=None)
+        
+    def communicate(self, input):
+        if self.p.poll() is not None:
+            logging.warn("SparqlRunner process died, respawning")
+            self.start_sparqlrunner()
+            
+        if input:
+            self.p.stdin.write(input)
+            self.p.stdin.write("\n\n")
+            self.p.stdin.flush()
+
+        out_lines, err_lines = [], []
+        func = functools.partial(read_lines, source=self.p.stdout, target=out_lines)
+        t = threading.Thread(target=func)
+        t.start()
+        while True:
+            line = self.p.stderr.readline().strip()
+            if not line:
+                raise Exception("Unexpected EOF!\n%s"%"\n".join(err_lines))
+            #print "E>", line.strip()
+            err_lines.append(line)
+            if line == 'SparqlRunner: READY':
+                break
+        t.join()
+        return "".join(out_lines)
+        
+        
+    def run(self, data, updates, parse=True):
+        if isinstance(data, Graph):
+            data = data.serialize(format="turtle")
+        input = json.dumps({"data" : data, "updates" : updates})
+        out = self.communicate(input)
+        if parse:
+            out = Graph().parse(data=out, format='n3')
+        return out
+
+        
+        
+_SINGLETON = None
+def _get_singleton():
+    global _SINGLETON
+    if _SINGLETON is None:
+        _SINGLETON = SparqlRunner()
+    return _SINGLETON
 
 def run(data, updates, parse=True):
     """
     Run the updates on the data and return the resulting triples
     """
+    return _get_singleton().run(data, updates, parse=True)
 
-    if isinstance(data, Graph):
-        data = data.serialize(format="turtle")
+def test():
+    
+    r = SparqlRunner()
 
-    jena = os.path.abspath(os.environ['JENA_HOME'])
-    import syntaxrules
-    runner = os.path.abspath(os.path.join(os.path.dirname(syntaxrules.__file__), "../sparqlrunner"))
-    classpath = "{jena}/lib/*:{runner}:{runner}/javax.json-1.0.4.jar".format(**locals())
-    cmd = "java -cp '{classpath}' SparqlRunner".format(**locals())
-    input = json.dumps({"data" : data, "updates" : updates})
-
-    p =subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate(input)
-
-    if 'SparqlRunner: DONE' not in err:
-        raise Exception("Problem in running updates:\n{err}".format(**locals()))
-    if parse:
-        out = Graph().parse(data=out, format='n3')
-    return out
-
-
-
-if __name__ == '__main__':
     data = "@prefix vCard:   <http://www.w3.org/2001/vcard-rdf/3.0#> .\n@prefix rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\n<http://somewhere/MattJones/>  vCard:FN   \"Matt Jones\" .\n<http://somewhere/MattJones/>  vCard:N    _:b0 .\n_:b0  vCard:Family \"Jones\" .\n_:b0  vCard:Given  \"Matthew\" .\n\n\n<http://somewhere/RebeccaSmith/> vCard:FN    \"Becky Smith\" .\n<http://somewhere/RebeccaSmith/> vCard:N     _:b1 .\n_:b1 vCard:Family \"Smith\" .\n_:b1 vCard:Given  \"Rebecca\" .\n\n<http://somewhere/JohnSmith/>    vCard:FN    \"John Smith\" .\n<http://somewhere/JohnSmith/>    vCard:N     _:b2 .\n_:b2 vCard:Family \"Smith\" .\n_:b2 vCard:Given  \"John\"  .\n\n<http://somewhere/SarahJones/>   vCard:FN    \"Sarah Jones\" .\n<http://somewhere/SarahJones/>   vCard:N     _:b3 .\n_:b3 vCard:Family  \"Jones\" .\n_:b3 vCard:Given   \"Sarah\" .\n\n"
     updates = ["PREFIX vCard: <http://www.w3.org/2001/vcard-rdf/3.0#>\nINSERT { ?x vCard:FN  \"Wouter\" } WHERE {?x vCard:FN \"John Smith\"}"]
 
-    run(data, updates)
+    g = r.run(data, updates)
+    g = r.run(data, updates)
+    print g
+    updatesfout = ["PREFXIX vCard: <http://www.w3.org/2001/vcard-rdf/3.0#>\nINSERT { ?x vCard:FN  \"Wouter\" } WHERE {?x vCard:FN \"John Smith\"}"]
+    try:
+        g = r.run(data, updatesfout)
+    except Exception, e:
+        logging.exception("ERROR")
+    print "----------"
+    g = r.run(data, updates)
+
+
+if __name__ == '__main__':
+    test()
